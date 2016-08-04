320843332c99dd3e917b5a232d0eb88e5534bcb1
Linh <nh.linh44@gmail.com>
2016-08-04 17:18:54 +0700

    signup edit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'new/index'
 
   get 'home/index' => 'login#index'
+  get 'home/index' => 'signup#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    match ':controller(/:action(/:id))', :via => [:get, :post]
